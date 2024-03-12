d6bbe3d7388b0ba0f695e09362c7bbf8bae13a56
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-12 12:56:31 -0300

    Mudancas rotas

--- AdPET/config/routes.php
+++ AdPET/config/routes.php
@@ -26,8 +26,8 @@ use Cake\Routing\RouteBuilder;
 
 /*
  * This file is loaded in the context of the `Application` class.
-  * So you can use  `$this` to reference the application class instance
-  * if required.
+ * So you can use  `$this` to reference the application class instance
+ * if required.
  */
 return function (RouteBuilder $routes): void {
     /*
@@ -75,6 +75,20 @@ return function (RouteBuilder $routes): void {
          * You can remove these routes once you've connected the
          * routes you want in your application.
          */
+        $builder->connect(
+            '/',
+            ['controller' => 'Pets', 'action' => 'index']
+        );
+
+        $builder->connect(
+            '/pet/:id',
+            ['controller' => 'Pets', 'action' => 'view'],
+            [
+                'pass' => ['id'],
+                'id' => '[0-9]+'
+            ]
+        );
+
         $builder->fallbacks();
     });
 
